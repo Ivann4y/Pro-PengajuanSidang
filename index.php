6e1170744db124f515f1a2e332774a6019d7ea2e
Nayaka <nayakaivanap@gmail.com>
2025-05-21 23:16:32 +0700

    Contoh Tambah file index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Pengajuan Sidang</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+
+<body class="bg-light">
+
+    <!-- Header Section -->
+    <div class="bg-primary text-white text-center py-5 position-relative" style="height: 50vh;">
+        <h3>Sistem Pengajuan Sidang</h3>
+        <h3>Politeknik Astra</h3>
+
+        <!-- Login Box -->
+        <div class="container mt-5 text-dark" style="z-index: 10;">
+            <div class="row justify-content-center">
+                <div class="col-md-5 text-center bg-white p-4 rounded shadow">
+                    <h1>Pengajuan</h1>
+                    <div class="d-grid gap-3">
+                        <div>
+                            Dosen
+                        </div>
+                        <div>
+                            Dosen
+                        </div>
+                        <div>
+                            Dosen
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
