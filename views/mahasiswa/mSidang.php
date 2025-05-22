e39fefa4222ec6f69236f64dbee95e430762532a
Yavuzzelim . <haarisnursalim@gmail.com>
2025-05-22 21:19:15 +0700

    cobaSidang

---
+++ views/mahasiswa/mSidang.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <title>Mahasiswa - Sidang</title>
+    <style>
+        .sideNav {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 10vw;
+            height: 100%;
+            background-color:rgb(16, 1, 230);
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="sideN">
+        
+    </div>av
+</body>
+</html>
