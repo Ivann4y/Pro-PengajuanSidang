d33c8123a72779da9521c442ff9a57b8f0f7aa16
Nayaka <nayakaivanap@gmail.com>
2025-05-23 16:19:47 +0700

    Menambahkan button pilihan

--- index.php
+++ index.php
@@ -6,34 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Pengajuan Sidang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
 </head>
 
 <body class="bg-light">
 
     <!-- Header Section -->
-    <div class="bg-primary text-white text-center py-5 position-relative" style="height: 50vh;">
-        <h3>Sistem Pengajuan Sidang</h3>
-        <h3>Politeknik Astra</h3>
+    <div class="text-white text-center py-5 position-relative" style="height: 50vh; background-color: rgb(67, 54, 240);">
+        <div class="mt-3">
+            <h2><strong>Sistem Pengajuan Sidang</strong></h2>
+            <h2><strong>Politeknik Astra</strong></h2>
+        </div>
 
         <!-- Login Box -->
-        <div class="container mt-5 text-dark" style="z-index: 10;">
+        <div class="container text-dark" style="z-index: 10; margin-top: 50px;">
             <div class="row justify-content-center">
-                <div class="col-md-5 text-center bg-white p-4 rounded shadow">
-                    <h1>Pengajuan</h1>
-                    <div class="d-grid gap-3">
+                <div class="col-md-5 text-center bg-white p-4 rounded rounded-5 shadow">
+                    <div class="my-2 p-2" style="color: rgb(67, 54, 240);">
+                        <h1>Masuk Sebagai</h1>
+                    </div>
+                    <div class="d-grid gap-3 mb-4 p-2">
                         <div>
-                            Dosen
+                            <button class="w-75 p-2 fw-bold fs-5" style="border: none; border-radius: 50px; background-color: #D9D9D9; color: #626262;">Dosen</button>
                         </div>
                         <div>
-                            Dosen
+                            <button class="w-75 p-2 fw-bold fs-5" style="border: none; border-radius: 50px; background-color: #D9D9D9; color: #626262;">Admin</button>
                         </div>
                         <div>
-                            Dosen
+                            <button class="w-75 p-2 fw-bold fs-5" style="border: none; border-radius: 50px; background-color: #D9D9D9; color: #626262;">Mahasiswa</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </body>
 
