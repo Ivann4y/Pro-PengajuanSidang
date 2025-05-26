1ea0956743a0bf8708f22bd2bc132cc289b0555f
hellwize <vanpendidikan09@gmail.com>
2025-05-26 16:18:00 +0700

    Evan:3

--- views/dosen/dBeranda.php
+++ views/dosen/dBeranda.php
@@ -1,3 +1,13 @@
-<Html:5>
     
-</Html:5>l
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+</head>
+<body>
+    <h1>halo</h1>
+
+</body>
+</html>
