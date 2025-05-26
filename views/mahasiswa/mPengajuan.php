7f29641ad3af6a2fa96d00f6ff5c0e74bf750eec
Vicky Prasetya <vprasetya79@gmail.com>
2025-05-27 05:32:29 +0700

    update pengajuan

--- views/mahasiswa/mPengajuan.php
+++ views/mahasiswa/mPengajuan.php
@@ -85,36 +85,42 @@
     <div class="sideNav"></div>
     <div class="container-fluid bodyContainer">
       <div class="row">
-        <h2 class="text-heading">
-          Nayaka Ivana Putra (Mahasiswa)
-        </h2>
-      </div><br><br>
-
-      <div class="row">
-        <div class="dropdown">
-          <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="ddMSidang">
-            Sidang TA
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#" id="ddMSidangMenu" onclick="switchMSidang();">Sidang Semester</a></li>
-          </ul>
+        <div class="col-12">
+          <h2 class="text-heading">Nayaka Ivana Putra (Mahasiswa)</h2>
         </div>
       </div><br><br>
 
       <div class="row">
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">No</th>
-              <th scope="col">Judul</th>
-              <th scope="col">Mata Kuliah</th>
-              <th scope="col">Dosen Pembimbing</th>
-              <th scope="col" class="text-center">Aksi</th>
-            </tr>
-          </thead>
-          <tbody id="mSidangTA"></tbody>
-          <tbody id="mSidangSem" style="display: none;"></tbody>
-        </table>
+        <div class="col-12 col-md-6">
+          <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="ddMSidang">
+              Sidang TA
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#" id="ddMSidangMenu" onclick="switchMSidang();">Sidang Semester</a></li>
+            </ul>
+          </div>
+        </div>
+      </div><br><br>
+
+      <div class="row">
+        <div class="col-12">
+          <div class="table-responsive">
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Judul</th>
+                  <th scope="col">Mata Kuliah</th>
+                  <th scope="col">Dosen Pembimbing</th>
+                  <th scope="col" class="text-center">Aksi</th>
+                </tr>
+              </thead>
+              <tbody id="mSidangTA"></tbody>
+              <tbody id="mSidangSem" style="display: none;"></tbody>
+            </table>
+          </div>
+        </div>
       </div>
 
       <button class="btn btn-primary tambah-sidang-btn" onclick="tambahData()">+ Tambah Sidang</button>
