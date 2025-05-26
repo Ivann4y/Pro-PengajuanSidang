d8ac2afd5b3b13f92ec6b9b6e58a8fbae5ef4d56
Yavuzzelim . <haarisnursalim@gmail.com>
2025-05-26 09:11:41 +0700

    update css

--- views/mahasiswa/mSidang.php
+++ views/mahasiswa/mSidang.php
@@ -10,6 +10,56 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <title>Mahasiswa - Sidang</title>
+    <style>
+        table {
+        border-spacing: 0 10px;
+        border-collapse: separate;
+        width: 100%;
+        }
+
+        thead {
+        border-bottom: 2px solid rgb(0, 0, 0) !important;
+        }
+
+        thead th {
+        padding: 12px 15px;
+        text-align: left;
+        }
+
+        thead th:nth-child(1) {
+        text-align: center;
+        width: 5%;
+        }
+
+        thead th:nth-child(2) {
+        width: 30%;
+        }
+
+        thead th:nth-child(3) {
+        width: 20%;
+        }
+
+        thead th:nth-child(4) {
+        width: 20%;
+        }
+
+        .isiTabel td {
+        padding: 12px 15px;
+        font-family: "Poppins";
+        font-weight: 400;
+        vertical-align: middle;
+        }
+
+        .isiTabel td:nth-child(1) {
+        border-radius: 20px 0 0 20px;
+        text-align: center;
+        }
+
+        .isiTabel td:nth-child(4) {
+        border-radius: 0 20px 20px 0;
+        text-align: center;
+        }
+    </style>
 </head>
 <body>
     <div class="container-fluid">
