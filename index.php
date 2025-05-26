b3312fc247d24e66803b66a181d54beb350c50c0
Nayaka <nayakaivanap@gmail.com>
2025-05-26 08:53:24 +0700

    coba CSS

--- index.php
+++ index.php
@@ -43,12 +43,17 @@
             background-color: rgb(67, 54, 240);
             height: 50vh;
         }
+
+        .fullscreen {
+            height: 100vh;
+            position: relative;
+        }
     </style>
 </head>
 
 <body>
     <!-- Fullscreen -->
-    <div style="height: 100vh; position: relative;">
+    <div class="fullscreen">
         <!-- Header Section -->
         <div class="bgBiru">
             <div class="text-white text-center py-5 fixed-top" style="z-index: 999;">
