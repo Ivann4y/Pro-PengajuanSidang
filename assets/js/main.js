23aa94221bfcb5c36cf09fdfcdc7c19856a3969e
widii11 <wdya1102@gmail.com>
2025-05-28 08:22:31 +0700

    comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
 // FUNGSI UNTUK ADMIN
 // --- Tulis fungsi di sini ---
 
-
 // FUNGSI UNTUK DOSEN
 // --- Tulis fungsi di sini ---
-
 
 // FUNGSI UNTUK MAHASISWA
 
 function switchMSidang() {
-    const msidang = document.getElementById("mSidangTA");
-    const msidang2 = document.getElementById("mSidangSem");
-    const msidangButton = document.getElementById("ddMSidang");
-    const msidangmenu = document.getElementById("ddMSidangMenu");
-    if (msidang && msidang2) {
-        if (msidang.style.display === "none" || getComputedStyle(msidang).display === "none") {
-            msidang.style.display = "table-row-group";
-            msidang2.style.display = "none";
-            msidangButton.innerText = "Sidang TA";
-            msidangmenu.innerText = "Sidang Semester";
-        } else {
-            msidang.style.display = "none";
-            msidang2.style.display = "table-row-group";
-            msidangButton.innerText = "Sidang Semester";
-            msidangmenu.innerText = "Sidang TA";
-        }
+  const msidang = document.getElementById("mSidangTA");
+  const msidang2 = document.getElementById("mSidangSem");
+  const msidangButton = document.getElementById("ddMSidang");
+  const msidangmenu = document.getElementById("ddMSidangMenu");
+  if (msidang && msidang2) {
+    if (
+      msidang.style.display === "none" ||
+      getComputedStyle(msidang).display === "none"
+    ) {
+      msidang.style.display = "table-row-group";
+      msidang2.style.display = "none";
+      msidangButton.innerText = "Sidang TA";
+      msidangmenu.innerText = "Sidang Semester";
+    } else {
+      msidang.style.display = "none";
+      msidang2.style.display = "table-row-group";
+      msidangButton.innerText = "Sidang Semester";
+      msidangmenu.innerText = "Sidang TA";
     }
+  }
 }
