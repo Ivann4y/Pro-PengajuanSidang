2646166702442727661d9d4024d27c048040d1c2
Nayaka <nayakaivanap@gmail.com>
2025-06-02 16:32:28 +0700

    Tambah foto di login, menambahkan login di setiap role

--- views/mahasiswa/mLogin.php
+++ views/mahasiswa/mLogin.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'mahasiswa') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Mahasiswa - Sistem Pengajuan Sidang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
     <style>
         body {
             font-family: 'Poppins', sans-serif;
@@ -46,8 +46,6 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'mahasiswa') {
             justify-content: center;
             align-items: center;
         }
-
-
 
         img {
             object-fit: cover;
