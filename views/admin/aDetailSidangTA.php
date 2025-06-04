47930ef7273d0d9b917bcef99e365fc9ce42b19b
imel <zahrahimeldaasari@gmail.com>
2025-06-04 16:14:06 +0700

    eval

--- views/admin/aDetailSidangTA.php
+++ views/admin/aDetailSidangTA.php
@@ -605,7 +605,7 @@
                 </li>
                 <li class="NavSide__sidebar-item ">
                     <b></b><b></b>
-                    <a href="mPengajuan.php"><span class="NavSide__sidebar-title fw-semibold">Evaluasi</span></a>
+                    <a href="aEvaluasi.php"><span class="NavSide__sidebar-title fw-semibold">Evaluasi</span></a>
                 </li>
                 <li class="NavSide__sidebar-item">
                     <b></b><b></b>
