a70ddff02b1ed4974413784947e8313dd12f03c9
Andien <andienswesty@gmail.com>
2025-06-09 15:12:40 +0700

    benerin modal keluar

--- views/admin/aPenjadwalan.php
+++ views/admin/aPenjadwalan.php
@@ -319,7 +319,7 @@ $filteredData = array_filter($data, function($entry) use ($selectedTipe, $status
         </li>
         <li class="NavSide__sidebar-item <?php echo (basename($_SERVER['PHP_SELF']) == 'logout.php' ? 'NavSide__sidebar-item--active' : ''); ?>">
             <b></b><b></b>
-                <a href="#" data-bs-toggle="modal" data-bs-target="#logout"><span class="NavSide__sidebar-title fw-semibold">Keluar</span></a>
+                <a href="logout.html" data-bs-toggle="modal" data-bs-target="#logABeranda"><span class="NavSide__sidebar-title fw-semibold">Keluar</span></a>
         </li>
     </ul>
 </div>
@@ -412,23 +412,26 @@ $filteredData = array_filter($data, function($entry) use ($selectedTipe, $status
     </div>
   </div>
 
-  <!-- Logout Modal -->
-  <div class="modal fade" id="logout" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-        <div class="modal-header mx-auto">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Perhatian!</h1>
-        </div>
-        <div class="modal-body mx-auto">
-             Apakah anda yakin ingin keluar?
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batalkan</button>
-            <button type="button" class="btn btn-success" onclick="window.location.href='../../logout.php'">Lanjutkan</button>
-        </div>
+   <!-- Modal keluar-->
+    <div class="modal fade" id="logABeranda" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div style="background-color: rgb(67, 54, 240);">
+                    <div class="modal-header">
+                        <h1 class="modal-title mx-auto fs-5 text-light" id="exampleModalLabel">Perhatian!</h1>
+                    </div>
+                </div>
+                <div class="modal-body mx-auto">
+                    Apakah anda yakin ingin keluar?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batalkan</button>
+                    <button type="button" class="btn btn-success" onclick="window.location.href='../../logout.php'">Lanjutkan</button>
+                </div>
+            </div>
         </div>
     </div>
-  </div>
+
   
   <!-- MODAL FOR SIDANG TA -->
   <div class="modal fade" id="penjadwalanSidangTAModal" aria-labelledby="penjadwalanSidangTAModalLabel" aria-hidden="true">
