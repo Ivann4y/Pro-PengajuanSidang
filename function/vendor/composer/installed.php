07b8dec188c36d92032904b7efcd7c45e62b5b6d
Vicky Prasetya <vprasetya79@gmail.com>
2025-06-15 02:31:14 +0700

    phpmailer include all file

---
+++ function/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pengajuan-sidang/function',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ebde3e42bb4cdd4059d5926d3d4ed4bda3ac6ed2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pengajuan-sidang/function' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ebde3e42bb4cdd4059d5926d3d4ed4bda3ac6ed2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
