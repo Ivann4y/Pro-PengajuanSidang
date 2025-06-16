28cd08bb8f5b2c5055b774933a517c294a403d2f
Ayashaaa210AP <arghaarybawa@gmail.com>
2025-06-16 13:51:00 +0700

    test

--- views/mahasiswa/mNilaiakhir.php
+++ views/mahasiswa/mNilaiakhir.php
@@ -255,7 +255,7 @@
         <div class="row mb-4 title-container">
             <div class="col-12">
                 <h2 class="main-title">
-                    Mahasiswa / Detail Evaluasi - Sistem Pengajuan Sidang
+                    Detail Evaluasi - Sistem Pengajuan Sidang
                 </h2>
             </div>
         </div>
