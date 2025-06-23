d173eac0124d77bcb843e1a34bfa35d4b8b27c23
Neromancy <andrewhermawan1705@gmail.com>
2025-06-23 14:30:38 +0700

    kjm

--- koneksi/koneksiAndrew.php
+++ koneksi/koneksiAndrew.php
@@ -1,4 +1,5 @@
 <?php
+
 $serverList = [
     "DESKTOP-5SK4QO2\\SQLEXPRESS",
     "DESKTOP-4E5I4LF\\SQLEXPRESS",
@@ -9,7 +10,7 @@ $serverList = [
     "MYBOOKHYPE\\SQLEXPRESS",
     "LAPTOP-7B4A2GEF\\SQLEXPRESS",
     "LAPTOP-6JC0DQKH\\SQLEXPRESS",
-    "WDYASCONNECT\\SQLEXPRESS", // replaced "/" with "\"
+    "WDYASCONNECT\\SQLEXPRESS", // replaced "/" with "\\"
     "RAKHAA\\SQLEXPRESS",
     "LAPTOP-IF4VFNR5\\SQLEXPRESS",
     "DESKTOP-QOLBRVJ\\SQLEXPRESS",
@@ -21,19 +22,39 @@ $serverList = [
 $connectionOptions = [
     "Database" => "SistemSidang1",
     "TrustServerCertificate" => true,
+    "LoginTimeout" => 2, // Try 2 seconds per server
     // "UID" => "username", "PWD" => "password", // Uncomment if needed
 ];
 
-$conn = null;
-foreach ($serverList as $serverName) {
+$conn = false;
+
+// 1. Try session server first
+if (isset($_SESSION['working_server'])) {
+    $serverName = $_SESSION['working_server'];
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if ($conn !== false) {
-        echo "Connected to: $serverName<br>";
-        break; // stop once successful
+        // Connected successfully using session server
+        // echo "Connected using session server: $serverName<br>";
+    } else {
+        // Remove invalid session server
+        unset($_SESSION['working_server']);
     }
 }
 
-// If still not connected
+// 2. If not connected, scan the list
+if ($conn === false) {
+    foreach ($serverList as $serverName) {
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if ($conn !== false) {
+            // Save working server in session
+            $_SESSION['working_server'] = $serverName;
+            // echo "Connected to: $serverName<br>";
+            break;
+        }
+    }
+}
+
+// 3. If still not connected, show error
 if ($conn === false) {
     echo "Koneksi Gagal ke semua server:<br>";
     die(print_r(sqlsrv_errors(), true));
