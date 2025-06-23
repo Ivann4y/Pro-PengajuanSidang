14b7e19261dc7aa9e7ff29db79722e217012c32b
yapusss <haarisnursalim@gmail.com>
2025-06-24 00:53:38 +0700

    mperbaikan

--- views/mahasiswa/mPerbaikan.php
+++ views/mahasiswa/mPerbaikan.php
@@ -131,7 +131,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="../../css/style.css" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+    <!-- <link rel="stylesheet" href="../../assets/css/mPerbaikan.css"> -->
 
     <style>
         /* === CSS LENGKAP DAN BERSIH UNTUK SIDEBAR DAN KONTEN (SUDAH DISESUAIKAN) === */
