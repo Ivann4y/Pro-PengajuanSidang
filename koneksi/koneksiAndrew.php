978fdca622d2395ca255d3910ea41a13707175e1
Neromancy <andrewhermawan1705@gmail.com>
2025-06-23 14:46:32 +0700

    gitignore

--- koneksi/koneksiAndrew.php
+++ koneksi/koneksiAndrew.php
@@ -1,62 +1,22 @@
 <?php
-
-$serverList = [
-    "DESKTOP-5SK4QO2\\SQLEXPRESS",
-    "DESKTOP-4E5I4LF\\SQLEXPRESS",
-    "HCCSSYY\\SQLEXPRESS",
-    "LAPTOP-7POM2U9J\\SQLEXPRESS",
-    "LAPTOP-EEE076JR\\SQLEXPRESS",
-    "INCOGNI-CAT\\SQLEXPRESS",
-    "MYBOOKHYPE\\SQLEXPRESS",
-    "LAPTOP-7B4A2GEF\\SQLEXPRESS",
-    "LAPTOP-6JC0DQKH\\SQLEXPRESS",
-    "WDYASCONNECT\\SQLEXPRESS", // replaced "/" with "\\"
-    "RAKHAA\\SQLEXPRESS",
-    "LAPTOP-IF4VFNR5\\SQLEXPRESS",
-    "DESKTOP-QOLBRVJ\\SQLEXPRESS",
-    "DESKTOP-FEILR9J\\SQLEXPRESS,59123", // optional port
-    "DESKTOP-M7H7C9C\\SQLEXPRESS01",
-    "BALTO\\SQLEXPRESS",
-];
+// Load the developer's local server name
+if (file_exists(__DIR__ . '/servername.php')) {
+    require __DIR__ . '/servername.php';
+} else {
+    die("No local servername defined. Please create servername.php");
+}
 
 $connectionOptions = [
     "Database" => "SistemSidang1",
     "TrustServerCertificate" => true,
-    "LoginTimeout" => 2, // Try 2 seconds per server
-    // "UID" => "username", "PWD" => "password", // Uncomment if needed
+    "LoginTimeout" => 2,
+    // "UID" => "username", "PWD" => "password",
 ];
 
-$conn = false;
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-// 1. Try session server first
-if (isset($_SESSION['working_server'])) {
-    $serverName = $_SESSION['working_server'];
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if ($conn !== false) {
-        // Connected successfully using session server
-        // echo "Connected using session server: $serverName<br>";
-    } else {
-        // Remove invalid session server
-        unset($_SESSION['working_server']);
-    }
-}
-
-// 2. If not connected, scan the list
 if ($conn === false) {
-    foreach ($serverList as $serverName) {
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if ($conn !== false) {
-            // Save working server in session
-            $_SESSION['working_server'] = $serverName;
-            // echo "Connected to: $serverName<br>";
-            break;
-        }
-    }
-}
-
-// 3. If still not connected, show error
-if ($conn === false) {
-    echo "Koneksi Gagal ke semua server:<br>";
+    echo "Koneksi Gagal ke server: $serverName<br>";
     die(print_r(sqlsrv_errors(), true));
 }
 
