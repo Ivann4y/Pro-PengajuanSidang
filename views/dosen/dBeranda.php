ad44587380cabf87bc17ae4fd9ff46aa91d6852d
Vicky Prasetya <vprasetya79@gmail.com>
2025-06-28 20:47:09 +0700

    aaaaku takuttttt, untuknya o tuaaaan

--- views/dosen/dBeranda.php
+++ views/dosen/dBeranda.php
@@ -81,8 +81,7 @@ $jumlahPenilaian = ($stmtPenilaian && $row = sqlsrv_fetch_array($stmtPenilaian))
                 </div>
             </div>
             <div class="dashboardTitle">Beranda Dosen</div>
-            <h2 class="welcomeText">Selamat Datang, Evan Wahyu!</h2>
-
+               <h2 class="welcomeText">Selamat Datang, <?php echo isset($_SESSION['user_data']['nama_dosen']) ? htmlspecialchars($_SESSION['user_data']['nama_dosen']) : 'Dosen'; ?>!</h2>
             <div class="row gy-4">
             <div class="col-lg-3">
                 <div class="mb-4">
