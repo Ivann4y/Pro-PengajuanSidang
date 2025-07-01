2f71ba806e38e5e73807d79ac148094313c563d9
naufalaf4@gmail.com <naufalaf4@gmail.com>
2025-07-01 11:47:04 +0700

    kwegvheuidsuhbif bjvifd bhv

--- views/dosen/dDokumenRevisi.php
+++ views/dosen/dDokumenRevisi.php
@@ -291,7 +291,8 @@ $namaFileRevisi = "dokumen_dummy_revisi.zip"; // Nama file default jika tidak ad
                 </div>
             </div>
 
-            <h5>Dokumen Revisi</h5>
+            <div>
+                <h4><b>Dokumen Revisi</b></h4>
             <div class="file-buttons-container d-flex flex-wrap">
                 <?php if (!empty($data_revisi['dok_revisi'])): ?>
                     <a href="../../uploadtesting/<?= $namaFileRevisi ?>" class="file-button" download>
