d6becb073ecc0c54a2afb4e2acd7a532e3864244
imel <zahrahimeldaasari@gmail.com>
2025-07-03 14:23:53 +0700

    555

--- views/dosen/dEvaluasiSidang.php
+++ views/dosen/dEvaluasiSidang.php
@@ -211,14 +211,15 @@ $namaPenguji_html = !empty($dosenPenguji) ? implode('<br>', array_map('htmlspeci
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evaluasi Sidang</title>
+       <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../extra/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
      <link rel="stylesheet" href="../../assets/css/dEvaluasiSidang.css">
-      <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../extra/style.css">
+   
 
 
    
