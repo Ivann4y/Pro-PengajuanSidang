ac884372f8cf7142ecc8631d7916534b08d44a96
Nayaka <nayakaivanap@gmail.com>
2025-07-10 10:20:41 +0700

    pindahin php ke php eksternal

--- views/admin/aEvaluasi.php
+++ views/admin/aEvaluasi.php
@@ -17,70 +17,8 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
   exit();
 }
 
-require "../../koneksi/koneksiAndrew.php";
-
-if (
-  !isset($_SESSION['id_sidang_aktif']) || !is_numeric($_SESSION['id_sidang_aktif']) ||
-  !isset($_SESSION['judul']) || empty($_SESSION['judul'])
-) {
-  $_SESSION['error_message'] = "Sidang tidak ditemukan atau belum dipilih.";
-  header("Location: aDaftarSidang.php");
-  exit();
-}
-
-$judulSidang = $_SESSION['judul'];
-$id_sidang = (int) $_SESSION['id_sidang_aktif'];
-
-$sql = "
-    SELECT 
-    ds.nomor_dosen,
-    d.nama_dosen,
-    ds.catatan_sidang,
-    ds.status_revisi,
-    ds.dok_revisi,
-    ds.nama_file,
-    p.peran_dosen
-FROM Detail_Sidang ds
-JOIN Dosen d ON ds.nomor_dosen = d.nomor_dosen
-JOIN Penjadwalan p ON ds.id_sidang = p.id_sidang AND ds.nomor_dosen = p.nomor_dosen
-WHERE ds.id_sidang = ?
-";
-
-$params = [$id_sidang];
-$stmt = sqlsrv_query($conn, $sql, $params);
-if ($stmt === false) {
-  die("Query gagal: " . print_r(sqlsrv_errors(), true));
-}
-
-$allRows = [];
-$statusList = [];
-
-while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-  $statusList[] = $row['status_revisi'];
-  $allRows[] = $row; // simpan baris-baris untuk ditampilkan nanti
-}
-
-// Reset ulang pointer untuk looping nanti
-sqlsrv_free_stmt($stmt);
-
-// Hitung status global
-if (!empty($allRows)) {
-  if (in_array('Ditolak', $statusList)) {
-    $statusRevisiGlobal = "Ditolak";
-    $badgeClass = "badge-danger";
-  } elseif (in_array('Pending', $statusList)) {
-    $statusRevisiGlobal = "Belum Disetujui";
-    $badgeClass = "badge-warning";
-  } else {
-    $statusRevisiGlobal = "Disetujui";
-    $badgeClass = "badge-success";
-  }
-} else {
-  // Kalau data kosong
-  $statusRevisiGlobal = "Tidak ada data";
-  $badgeClass = "badge-secondary";
-}
-
+require $path_to_root . "koneksi/koneksiAndrew.php";
+include $path_to_root . "control/admin/aEvaluasi_queries.php";
 ?>
 <!DOCTYPE html>
 <html lang="id">
