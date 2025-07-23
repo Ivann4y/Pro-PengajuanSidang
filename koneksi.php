37e6fae88a73e69a8bebef92eb0a5753eb2ff719
Nayaka <nayakaivanap@gmail.com>
2025-07-23 20:35:45 +0700

    ganti nama database

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
     // $serverName = "celsi.tail7d1f1d.ts.net,1433";
     $serverName = "MYBOOKHYPE\SQLEXPRESS"; // Ganti dengan nama server SQL Server Anda
 $connectionOptions = [
-    "Database" => "SistemSidang1",
+    "Database" => "SistemSidang",
     // "Uid" => "sqladmin",  
     // "PWD" => "sistemsidang",  
     "TrustServerCertificate" => true,
