da6efa7a1a6d09fe2301cded2c7ba1934d306566
IncognicatJess <jaisyunurm@gmail.com>
2025-07-23 17:51:19 +0700

    Perbaikan judul lupa sandi role - modularisasi toLupaPassword()

--- views/mahasiswa/mLogin.php
+++ views/mahasiswa/mLogin.php
@@ -116,10 +116,8 @@ $role = "mahasiswa";
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-    <script>
-        function toLupaPassword() {
-            window.location.href = "../../views/lupaPassword.php?role=<?= $role ?>";
-        }
+   <script>
+    const ROLE = "<?= htmlspecialchars($role) ?>";
     </script>
     <script src="../../assets/js/login.js"></script>
 </body>
